ba8a9f987c0f0b2fefeefd45f57e19142135b9b9
Kevin Oliveira <kevinobruno@gmail.com>
2015-06-12 09:22:03 -0400

    adicionado tableview

--- foodSurvivel2/FoodInfoScene.m
+++ foodSurvivel2/FoodInfoScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "FoodInfoScene.h"
+
 #define NODENAME_BACKBUTTON         @"backButton"
 
 @interface FoodInfoScene ()
@@ -36,15 +37,15 @@
 @implementation FoodInfoScene
 
 - (void)didMoveToView:(SKView *)view {
-    _title = [[NSMutableArray alloc]initWithObjects:@"Banana", nil];
-    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(50, 0, self.frame.size.width, self.frame.size.height)];
+    _title = [[NSMutableArray alloc]initWithObjects:@"Cenoura",@"Laranja",@"Maça", nil];
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(50, 10, self.frame.size.width, self.frame.size.height)];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     _tableView.hidden = NO;
     _tableView.scrollEnabled = YES;
     _tableView.allowsMultipleSelectionDuringEditing = NO;
     _tableView.allowsSelection = NO;
-    _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.view addSubview:_tableView];
 }
 
@@ -59,9 +60,60 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellidentifier];
     }
+    //adicionado imagem na tableview
+    
+    UIImageView *tempImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background1.png"]];
+    [tempImageView setFrame:self.tableView.frame];
+    self.tableView.backgroundView = tempImageView;
     
     cell.textLabel.text = _title[indexPath.row];
+    cell.textLabel.font=[UIFont fontWithName:@"Courier" size:12];
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.backgroundColor = [UIColor colorWithWhite:0 alpha:0.0];
+    
+    UIImage *imageCenoura=[UIImage imageNamed:@"cenoura.png"];
+    UIImage *imageMaca=[UIImage imageNamed:@"maca.png"];
+    UIImage *imageLaranja=[UIImage imageNamed:@"laranja.png"];
+    
+    
+    switch (indexPath.row) {
+            
+        case 0:
+            cell.imageView.image=imageCenoura;
+            break;
+        case 1:
+            cell.imageView.image=imageLaranja;
+            
+            break;
+        case 2:
+            cell.imageView.image=imageMaca;
+            
+            break;
+            
+        default:
+            break;
+    }
     return cell;
+}
+
+//Mudar o tamanho das células
+- (CGFloat)tableView:(UITableView *)aTableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat retVal =0.0f;
+    
+    if(indexPath.section==0 || indexPath.section==1 )
+    {
+        if(indexPath.row==0 || indexPath.row==1)
+        {
+            retVal=50.0f;//the height you want.
+        }
+        else
+            retVal=50.0f;
+    }
+    else
+        retVal=50.0f;
+    
+    return retVal;
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
