9d677295719b9caf1e86ba94e468c24e202c863d
Eduarda Pinheiro <eduardapinh@MacBook-Pro-de-Eduarda.local>
2015-06-17 08:28:26 -0400

    Ajustes na tableview. Adição de imagens como exemplo.

--- foodSurvivel2/FoodInfoScene.m
+++ foodSurvivel2/FoodInfoScene.m
@@ -7,6 +7,7 @@
 //
 
 #import "FoodInfoScene.h"
+
 #define NODENAME_BACKBUTTON         @"backButton"
 
 @interface FoodInfoScene ()
@@ -36,15 +37,15 @@
 @implementation FoodInfoScene
 
 - (void)didMoveToView:(SKView *)view {
-    _title = [[NSMutableArray alloc]initWithObjects:@"Banana", nil];
-    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(50, 0, self.frame.size.width, self.frame.size.height)];
+    _title = [[NSMutableArray alloc]initWithObjects:@"Cenouras contém vitamina A, essa vitamina ajuda melhorar a visão noturna.",@"As vezes doces ou azedas as laranjas contém vitaminas que contribuem para evitar uma série de variedades de câncer e protege contra a gripe.",@"Rico em vitamina C, brocólis ajuda na visão de detalhes, tanto a visão de longe como a visão de perto.", @"Sanduíches são deliciosos, mas não coma muito pois contém gorduras ruins que pode causar obesidade, doenças no coração e diabetes.",@"Chocolates são deliciosos mas comer muito pode provocar diabetes que é o aumento de açúcar no sangue.",@"Açúcar em excesso é um perigo. Cuidado! pirulitos causam cáries nos dentes.",nil];
+    _tableView = [[UITableView alloc] initWithFrame:CGRectMake(50, 10, self.frame.size.width, self.frame.size.height)];
     _tableView.delegate = self;
     _tableView.dataSource = self;
     _tableView.hidden = NO;
     _tableView.scrollEnabled = YES;
     _tableView.allowsMultipleSelectionDuringEditing = NO;
     _tableView.allowsSelection = NO;
-    _tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    _tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     [self.view addSubview:_tableView];
 }
 
@@ -59,9 +60,86 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:cellidentifier];
     }
+    //adicionado imagem na tableview
+    
+    UIImageView *tempImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background1.png"]];
+    [tempImageView setFrame:self.tableView.frame];
+    self.tableView.backgroundView = tempImageView;
     
     cell.textLabel.text = _title[indexPath.row];
+    
+    cell.textLabel.font=[UIFont fontWithName:@"Courier" size:12];
+    cell.textLabel.numberOfLines=5;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
+    cell.backgroundColor = [UIColor colorWithWhite:0 alpha:0.0];
+    
+
+    
+    UIImage *imageCenoura=  [UIImage imageNamed:@"cenoura.png"];
+    UIImage *imageLaranja=  [UIImage imageNamed:@"laranja.png"];
+    UIImage *imageBrocolis= [UIImage imageNamed:@"brocolis.png"];
+    UIImage *imageSanduiche=[UIImage imageNamed:@"sanduiche.png"];
+    UIImage *imageChocolate=[UIImage imageNamed:@"chocolate"];
+    UIImage *imagePirulito= [UIImage imageNamed:@"pirulito"];
+    
+    
+
+    
+
+    
+    switch (indexPath.row) {
+            
+        case 0:
+            cell.imageView.image=imageCenoura;
+            break;
+        case 1:
+            cell.imageView.image=imageLaranja;
+            
+            break;
+        case 2:
+            cell.imageView.image=imageBrocolis;
+            
+            break;
+        case 3:
+            cell.imageView.image=imageSanduiche;
+            break;
+            
+        case 4:
+            cell.imageView.image=imageChocolate;
+            break;
+        case 5:
+            cell.imageView.image=imagePirulito;
+            break;
+            
+        default:
+            break;
+
+            
+    }
     return cell;
+}
+
+
+
+
+//Mudar o tamanho das células
+- (CGFloat)tableView:(UITableView *)aTableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    CGFloat retVal =0.0f;
+    
+    if(indexPath.section==0 || indexPath.section==1 )
+    {
+        if(indexPath.row==0 || indexPath.row==1)
+        {
+            retVal=50.0f;//the height you want.
+        }
+        else
+            retVal=50.0f;
+    }
+    else
+        retVal=50.0f;
+    
+    return retVal;
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
@@ -74,5 +152,7 @@
         [self.scene.view presentScene:[StartScene unarchiveFromFile:@"StartScene"]];
     }
 }
+
+
 
 @end
