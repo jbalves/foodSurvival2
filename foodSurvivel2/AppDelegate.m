c039c28e7f53bcea64398a407ba60bdaee6c257e
Kevin Oliveira <kevinobruno@gmail.com>
2015-06-22 09:57:00 -0400

    ajustes de fisica na primeira fase

--- foodSurvivel2/AppDelegate.m
+++ foodSurvivel2/AppDelegate.m
@@ -16,30 +16,42 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    //PERMISSION TO SHOW NOTIFICATION
+    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]) {
+        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings
+                                                                             settingsForTypes:UIUserNotificationTypeAlert |
+                                                                             UIUserNotificationTypeSound |
+                                                                             UIUserNotificationTypeBadge categories:nil]];
+    }
+    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    
+    //NOTIFICATIONS
+    NSDate *alarmTime = [NSDate dateWithTimeIntervalSinceNow:21600];
+    UIApplication *app = [UIApplication sharedApplication];
+    UILocalNotification *note = [[UILocalNotification alloc] init];
+    
+    if (note) {
+        note.fireDate = alarmTime;
+        note.timeZone = [NSTimeZone defaultTimeZone];
+        note.repeatInterval = NSCalendarUnitHour*6;
+        note.alertBody = @"Jack está ficando gordo. Ajude-o a perder peso.";
+        [app setScheduledLocalNotifications:[NSArray arrayWithObject:note]];
+    }
+    
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    UIApplication *app = [UIApplication sharedApplication];
+    NSArray *oldNotifications = [app scheduledLocalNotifications];
+    
+    if ([oldNotifications count] > 0) {
+        [app cancelAllLocalNotifications];
+    }
 }
 
 @end
