3c194c13b325de5b5065f260af69380e427e9d88
Kevin Oliveira <kevinobruno@gmail.com>
2015-06-24 16:52:15 -0400

    ajustes de cutscenes

--- foodSurvivel2/CutSceneLevel1.h
+++ foodSurvivel2/CutSceneLevel1.h
@@ -1,16 +1,15 @@
 //
-//  StageSelectScene.h
+//  CutSceneLevel1.h
 //  foodSurvivel2
 //
-//  Created by Jeferson Barros Alves on 17/06/15.
+//  Created by Jeferson Barros Alves on 19/06/15.
 //  Copyright © 2015 edu FUCAPI. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 #import <SpriteKit/SpriteKit.h>
 #import "Level1Scene.h"
-#import "CutSceneLevel1.h"
 
-@interface StageSelectScene : SKScene
+@interface CutSceneLevel1 : SKScene
 
 @end
