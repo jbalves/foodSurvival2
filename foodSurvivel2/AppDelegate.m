652e85949e5c05ff372870bc6cbe733a6aeaa01a
Kevin Oliveira <kevinobruno@gmail.com>
2015-06-24 19:13:49 -0400

    finalizacao das cutscenes

--- foodSurvivel2/AppDelegate.m
+++ foodSurvivel2/AppDelegate.m
@@ -18,12 +18,12 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
     //PERMISSION TO SHOW NOTIFICATION
-    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]) {
-        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings
-                                                                             settingsForTypes:UIUserNotificationTypeAlert |
-                                                                             UIUserNotificationTypeSound |
-                                                                             UIUserNotificationTypeBadge categories:nil]];
-    }
+//    if ([UIApplication instancesRespondToSelector:@selector(registerUserNotificationSettings:)]) {
+//        [[UIApplication sharedApplication] registerUserNotificationSettings:[UIUserNotificationSettings
+//                                                                             settingsForTypes:UIUserNotificationTypeAlert |
+//                                                                             UIUserNotificationTypeSound |
+//                                                                             UIUserNotificationTypeBadge categories:nil]];
+//    }
     
     return YES;
 }
@@ -31,27 +31,27 @@
 - (void)applicationDidEnterBackground:(UIApplication *)application {
     
     //NOTIFICATIONS
-    NSDate *alarmTime = [NSDate dateWithTimeIntervalSinceNow:21600];
-    UIApplication *app = [UIApplication sharedApplication];
-    UILocalNotification *note = [[UILocalNotification alloc] init];
-    
-    if (note) {
-        note.fireDate = alarmTime;
-        note.timeZone = [NSTimeZone defaultTimeZone];
-        note.repeatInterval = NSCalendarUnitHour*6;
-        note.alertBody = @"Jack está ficando gordo. Ajude-o a perder peso.";
-        [app setScheduledLocalNotifications:[NSArray arrayWithObject:note]];
-    }
+//    NSDate *alarmTime = [NSDate dateWithTimeIntervalSinceNow:21600];
+//    UIApplication *app = [UIApplication sharedApplication];
+//    UILocalNotification *note = [[UILocalNotification alloc] init];
+//    
+//    if (note) {
+//        note.fireDate = alarmTime;
+//        note.timeZone = [NSTimeZone defaultTimeZone];
+//        note.repeatInterval = NSCalendarUnitHour*6;
+//        note.alertBody = @"Jack está ficando gordo. Ajude-o a perder peso.";
+//        [app setScheduledLocalNotifications:[NSArray arrayWithObject:note]];
+//    }
     
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    UIApplication *app = [UIApplication sharedApplication];
-    NSArray *oldNotifications = [app scheduledLocalNotifications];
-    
-    if ([oldNotifications count] > 0) {
-        [app cancelAllLocalNotifications];
-    }
+//    UIApplication *app = [UIApplication sharedApplication];
+//    NSArray *oldNotifications = [app scheduledLocalNotifications];
+//    
+//    if ([oldNotifications count] > 0) {
+//        [app cancelAllLocalNotifications];
+//    }
 }
 
 @end
